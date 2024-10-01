53ccfc733c0afca412219d0d5e1159fdbb2ebc26
xkylz1 <fauzansaputra741@gmail.com>
2024-10-01 21:40:51 +0800

    learn api

--- Chapter-3/30-09-2024/server.js
+++ Chapter-3/30-09-2024/server.js
@@ -6,8 +6,8 @@ const app = express();
 // default URL = Health check
 app.get("/", (req, res) => {
   res.status(200).json({
-    "status":"Success",
-    "message":"Application is running good..."
+    status: "Success",
+    message: "Application is running good...",
   });
 });
 
@@ -17,16 +17,28 @@ app.get("/fauzan", (req, res) => {
   });
 });
 
+// /api/v1/(collection) => collection harus jamak (s), ex cars
+app.get("/api/v1/cars", (req, res) => {
+const cars=JSON.parse(fs.readFileSync(`${__dirname}/assets/data/cars.json`,"utf-8")) 
+
+  res.status(200).json({
+    status: "Success",
+    message: "success get cars data",
+    isSuccess: true,
+    data:cars,
+  });
+});
+
 // middleware / handler url yang tidak ada
-app.use((req, res, next)=>{
-    res.status(404).json({
-        "status": "Failed",
-        "message":"API not exist"
-    })
-})
-
-
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "Failed",
+    message: "API not exist",
+  });
+});
 
 app.listen("3000", () => {
   console.log("Start aplikasi di port 3000");
 });
+
+// backend indentasinya 2
