4559d9d482ecc2533b0b6b91cb16947137da19fa
ojan <fauzansaputra741@gmail.com>
2024-10-03 20:31:47 +0800

    selasa

--- Chapter-3/30-09-2024/server.js
+++ Chapter-3/30-09-2024/server.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
+// const http = require("http")
 const express = require("express");
 
 const app = express();
+
+// middleware untuk membaca json dari request body(client, FE dll) ke kita
+app.use(express.json());
 
 // default URL = Health check
 app.get("/", (req, res) => {
@@ -11,34 +15,63 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/fauzan", (req, res) => {
+// kalau HTTP module kan if(req.url === / "Tegar") {}
+app.get("/tegar", (req, res) => {
   res.status(200).json({
-    "message    ": "ping successfully!",
+    message: "Ping Successfully !",
   });
 });
 
-// /api/v1/(collection) => collection harus jamak (s), ex cars
-app.get("/api/v1/cars", (req, res) => {
-const cars=JSON.parse(fs.readFileSync(`${__dirname}/assets/data/cars.json`,"utf-8")) 
+const cars = JSON.parse(
+  fs.readFileSync(`${__dirname}/assets/data/cars.json`, "utf-8")
+);
 
+// /api/v1/(collection nya) => collection nya ini harus JAMAK (s)
+app.get("/api/v1/cars", (req, res) => {
   res.status(200).json({
     status: "Success",
-    message: "success get cars data",
+    message: "Success get cars data",
     isSuccess: true,
-    data:cars,
+    totalData: cars.length,
+    data: {
+      cars,
+    },
   });
 });
 
-// middleware / handler url yang tidak ada
+// response.data.cars
+
+app.post("/api/v1/cars", (req, res) => {
+  // insert into ......
+  const newCar = req.body;
+
+  cars.push(newCar);
+
+  fs.writeFile(
+    `${__dirname}/assets/data/cars.json`,
+    JSON.stringify(cars),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        message: "Success add new car data",
+        isSuccess: true,
+        data: {
+          car: newCar,
+        },
+      });
+    }
+  );
+});
+
+// middleware / handler untuk url yang tidak dapat diakses karena memang tidak ada di aplikasi
+// membuat middleware = our own middleware
 app.use((req, res, next) => {
   res.status(404).json({
     status: "Failed",
-    message: "API not exist",
+    message: "API not exist !!!",
   });
 });
 
-app.listen("3000", () => {
-  console.log("Start aplikasi di port 3000");
+app.listen("8000", () => {
+  console.log("start aplikasi kita di port 8000");
 });
-
-// backend indentasinya 2
